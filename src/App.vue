24e00b77ccfff3fd43e3e95d88deda9ef9049929
Charlot Raveloarison <charlotclarele@gmail.com>
2023-11-05 08:46:32 +0300

    use watchers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import MyComponent from './components/MyComponent.vue';
 import ProductForm from './components/ProductForm.vue';
 </script>
 
 <template>
   <ProductForm />
+  <MyComponent />
 </template>
