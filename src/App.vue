ffd27e380db9b61c9343c0ecc90ffa96df61647a
Charlot Raveloarison <charlotclarele@gmail.com>
2023-11-05 11:26:37 +0300

    set modal

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@ import MyComponent from './components/MyComponent.vue';
 import ProductForm from './components/ProductForm.vue';
 import ChildComp from './components/ChildComp.vue';
 import GridFilterComponent from './components/GridFilterComponent.vue';
+import Modal from './components/Modal.vue';
 
 const greeting = ref("Hello from parent")
 const childMsg = ref("No child msg yet")
+const showModal = ref(false)
 </script>
 
 <template>
@@ -16,4 +18,14 @@ const childMsg = ref("No child msg yet")
   <p>{{ childMsg }}</p>
   <ChildComp>{{ greeting }}</ChildComp>
   <GridFilterComponent />
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </modal>
+  </Teleport>
 </template>
